05ffe4419452f239893c871a2be7271a51fd20f3
Marco Caberletti <marco.caberletti@gmail.com>
2017-06-04 16:33:18 +0200

    Add FC26 script

--- utils/setup-suse.sh
+++ utils/setup-suse.sh
@@ -10,31 +10,27 @@ package_list=\
  git \
  gnome-subtitles \
  google-chrome-stable \
- gsmartcontrol \
- gstreamer-plugins-bad-free \
  gstreamer-plugins-base \
  gstreamer-plugins-good \
  gstreamer-plugins-ugly \
  gstreamer-plugins-good \
- gstreamer-plugins-good-extras \
  k3b \
  libreoffice-base \
- libcurl \
  lyx \
  nautilus-extension-dropbox \
  nmap \
- pygpgme \
+ python-gpgme \
  smartmontools \
  sqlitebrowser \
  sysstat \
  thunderbird \
- tomboy \
  unrar \
  unzip \
  vlc"
 
+wget https://dl.google.com/linux/linux_signing_key.pub
+rpm --import linux_signing_key.pub
+
 zypper install -y /home/marco/programmi/google-chrome-stable_current_x86_64.rpm
 zypper update -y && zypper install -y $package_list
-
-
 
